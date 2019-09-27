0c247ee2205af5c18e2e862fc5fc0baa4017c257
msalazarv <mswillsa@gmail.com>
2019-09-27 09:59:46 -0600

    Initial commit

---
+++ app/src/main/java/com/example/primeravance/Subneteo.java
@@ -0,0 +1,14 @@
+package com.example.primeravance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Subneteo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subneteo);
+    }
+}
